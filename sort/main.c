9ad92c383623d02e1e082a9487b513a2e4d5eb0f
20161104613 <a20161104613@admindeiMac-34.local>
2017-06-12 11:35:04 +0800

    排序
    冒泡排序  10个数

--- sort/main.c
+++ sort/main.c
@@ -7,9 +7,32 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int a[10];
+    int j,i,t;;
+    printf("请输入10个数：\n");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<9;j++)
+    {
+        for(i=0;i<=8-j;i++)
+        {
+        if(a[i]>a[i+1])
+        {
+            t=a[i];
+            a[i]=a[i+1];
+            a[i+1]=t;
+        }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d",a[i]);
+        printf(" ");
+    }
+    printf("\n");
     return 0;
 }
