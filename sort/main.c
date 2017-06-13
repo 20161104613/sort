d0e17a4d4a34fa82803051ff9b41ccf93b679146
20161104613 <a20161104613@admindeiMac-34.local>
2017-06-13 09:47:35 +0800

    读取文件排序
    读取文件排序

--- sort/main.c
+++ sort/main.c
@@ -11,10 +11,19 @@ int main()
 {
     int a[10];
     int j,i,t;;
-    printf("请输入10个数：\n");
+    FILE *fp1,*fp2;
+    //char ch;
+    //Users/a20161104613/Desktop/sort\ 01/sort\ 01.xcodeproj
+    fp1=fopen("//Users//a20161104613//Desktop//sort//numberout.txt","w");
+    fp2=fopen("//Users//a20161104613//Desktop//sort//number.txt","r");
+    if(fp2 ==NULL)
+    {
+        printf("打开文件错误！");
+    }
+    //printf("请输入10个数：\n");
     for(i=0;i<10;i++)
     {
-        scanf("%d",&a[i]);
+        fscanf(fp2,"%d",&a[i]);
     }
     for(j=0;j<9;j++)
     {
@@ -31,8 +40,10 @@ int main()
     for(i=0;i<10;i++)
     {
         printf("%d",a[i]);
+        fprintf(fp1,"%d ",a[i]);
         printf(" ");
     }
     printf("\n");
+    fprintf(fp1,"\n");
     return 0;
 }
