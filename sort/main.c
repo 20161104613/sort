ac349d9e0ef9a916e9a1d79a4af243ee94c33e52
20161104613 <1772569458@qq.com>
2017-06-16 17:19:40 +0800

    最后一次
    只能十个数排序。成功

--- sort/main.c
+++ sort/main.c
@@ -9,13 +9,13 @@
 #include <stdio.h>
 int main()
 {
-    int a[5][10];
+    int a[10];
     int j,i,t;;
     FILE *fp1,*fp2;
     //char ch;
     //Users/a20161104613/Desktop/sort\ 01/sort\ 01.xcodeproj
-    fp1=fopen("//Users//a20161104613//Desktop//sort//numberout.txt","w");
-    fp2=fopen("//Users//a20161104613//Desktop//sort//number.txt","r");
+    fp1=fopen("//Users//a20161104613//Documents//GitHub//sort//numberout.txt","w");
+    fp2=fopen("//Users//a20161104613//Documents//GitHub//sort//number.txt","r");
     if(fp2 ==NULL)
     {
         printf("打开文件错误！");
@@ -23,7 +23,7 @@ int main()
     //printf("请输入10个数：\n");
     for(i=0;i<10;i++)
     {
-        while(fscanf(fp2,"%d",&a[0][i]);
+        fscanf(fp2,"%d",&a[i]);
     }
     for(j=0;j<9;j++)
     {
